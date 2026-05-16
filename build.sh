8198cdd5549f5bed735e16d313961de003a407b7
arigelbard <ag2635@georgetown.edu>
2026-05-16 09:28:52 -0400

    Created index and images folder

---
+++ build.sh
@@ -0,0 +1,46 @@
+####################################################################
+git pull
+
+# SET PERMISSIONS FOR ROOT SITE FILES
+chmod 644 index.html
+for i in $(find images -type f); do chmod 644 $i; done
+chmod 755 images
+
+# GITHUB SYNC
+printf 'Would you like to push to GITHUB? (y/n)? '
+read answer
+if [ "$answer" != "${answer#[Yy]}" ] ;then 
+
+    git config http.postBuffer 20242880000
+
+    # PULL CLOUD REPO TO LOCAL
+    git pull 
+
+    # SYNC TO LOCAL REPO TO CLOUD 
+    read -p 'ENTER MESSAGE: ' message
+    echo "commit message = "$message; 
+    git add ./; 
+    # MAIN BRANCH
+    git commit -m "$message"; 
+
+    # PUSH NON-MAIN BRANCH
+    #git push  -u origin w05-draft
+
+    # PUSH MAIN BRANCH
+    git push
+
+else
+    echo NOT PUSHING TO GTIHUB!
+fi
+
+
+###################################################################################
+# Prompt user to push to website
+printf 'Do you want to push the website to the Georgetown University domains folder? (y/n)'
+read answer 
+
+if [ "$answer" != "${answer#[Yy]}" ] ;then 
+    rsync -avz --delete -e "ssh -i ~/.ssh/id_rsa" _site/* arigelba@gtown03.reclaimhosting.com:/home/arigelba/public_html/index.html
+else 
+    echo NOT PUSHING TO WEBSITE!
+fi
